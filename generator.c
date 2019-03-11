c7c41195f7f8b82e8c3d1e81f9116ff2336e9c52
Benjamin Shanahan <benshanahan1@gmail.com>
2019-03-11 02:38:08 +0000

    add basic stdout waveform generator

---
+++ generator.c
@@ -0,0 +1,32 @@
+/** Generator.
+ * 
+ * Generate waveform data and write it to `stdout`.
+ */
+
+#include <stdio.h>
+#include <math.h>
+#include <unistd.h>
+
+#define FS 1000.0  // simulated sampling rate
+#define SCALE 100000.0  // max (and min) value output value
+#define SLEEP_INTERVAL (1 / FS)*1000000
+
+/** Generate data point. */
+long generate_sine(long x) {
+	double zzz = sin(((double) x) / FS);
+
+	// Scale generated value 
+	return (long) (zzz * SCALE);
+}
+
+int main(int argc, char *argv[]) {
+	long counter = 0;
+
+	for (;;) {
+		fprintf(stdout, "%ld\n", generate_sine(counter));
+		counter ++;
+		usleep(SLEEP_INTERVAL);
+	}
+
+	return 0;
+}
